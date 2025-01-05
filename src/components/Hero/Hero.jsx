08237a1a7df3193e1a25b987620c7b038be94948
ethanluc7 <140726164+ethanluc7@users.noreply.github.com>
2025-01-05 13:14:13 -0500

    Add files via upload

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,21 @@
-import React from 'react';
-import './Hero.css';
-import '../../index.css';
-import anime from 'animejs/lib/anime.es.js';
+import React from "react";
+import "./Hero.css";
+import "../../index.css";
+import anime from "animejs/lib/anime.es.js";
 
-const Hero = ( {color} ) => {
-
-
-
-
-
+const Hero = ({ color }) => {
   return (
     <section>
-
-      <div className = "hero"> 
-          <div className = "container">
-
-              <div className = "main-text">
-                  <h1 className = {`text ${color}`}>hi, i'm ethan luc</h1>
-                  <h2 className = {`text sub ${color}`}>math student @ uw</h2>
-              </div>
-
+      <div className="hero">
+        <div className="hero-container">
+          <div className="main-text">
+            <h1 className={`text ${color}`}>hi, i'm ethan luc</h1>
+            <h2 className={`text sub ${color}`}>math student @ uw</h2>
           </div>
-          
+        </div>
       </div>
-
-
-
-
     </section>
+  );
+};
 
-     
-      
-
-  )
-}
-
-export default Hero
+export default Hero;
