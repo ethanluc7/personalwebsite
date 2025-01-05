00c66e5d62f96dc35ddc661d9801d859400b6482
ethanluc7 <140726164+ethanluc7@users.noreply.github.com>
2025-01-05 13:14:32 -0500

    Add files via upload

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,55 +1,91 @@
-import anime from 'animejs/lib/anime.es.js';
-import './NavBar.css';
-import '../../index.css';
-import { NavLink } from 'react-router-dom';
-import circle from '../../assets/circle.svg';
-import email from '../../assets/email.png';
-import githubLogo from '../../assets/githublogo.png';
-import linkedInLogo from '../../assets/linkedinlogo.png';
-import { HashLink as Link } from 'react-router-hash-link';
+import anime from "animejs/lib/anime.es.js";
+import "./NavBar.css";
+import "../../index.css";
+import email from "../../assets/email.png";
+import githubLogo from "../../assets/githublogo.png";
+import linkedInLogo from "../../assets/linkedinlogo.png";
+import { HashLink } from "react-router-hash-link";
+import { useEffect } from "react";
 
-const NavBar = ( {color} ) => {
+const NavBar = ({ color }) => {
+  useEffect(() => {
+    const tl = anime.timeline();
 
+    tl.add({
+      targets: ".icons, .rose, .first",
+      translateY: [-50, 0],
+      opacity: [0, 1],
+      easing: "easeOutQuad",
+      duration: 700,
+      delay: anime.stagger(240),
+    }).add({
+      targets: ".text",
+      translateY: [50, 0],
+      opacity: [0, 1],
+      easing: "easeOutQuad",
+      duration: 700,
+      delay: anime.stagger(240),
+    });
+  }, []);
 
+  // anime({
+  //     targets: ".header ",          // Target the element with the class "box"
+  //     translateY: [-50, 0],    // Slide down from -100px to its original position
+  //     opacity: [0, 1],          // Fade in from 0 to 1
+  //     duration: 700,           // Animation duration in milliseconds
+  //     easing: "easeOutQuad",    // Smooth easing
+  //   });
+  // }, []);
 
+  // hover effect over icons in navigation bar
 
   return (
-    
     <nav>
-        <div className= "container">
-            <div className = "header">
+      <div className="container">
+        <div className="header">
+          <div className="header-left">
+            <a href="https://github.com/ethanluc7">
+              <img className="icons" src={githubLogo} alt="github logo" />
+            </a>
 
-                <div className= "header-left">
+            <a href="https://www.linkedin.com/in/ethan-luc-29035325a/">
+              <img className="icons" src={linkedInLogo} alt="linkedin logo" />
+            </a>
 
-                    <a href ="https://github.com/ethanluc7">
-                        <img className = "icons" src = {githubLogo} alt = 'github logo'/>
-                    </a>
+            <a href="mailto:ethan.q.luc@gmail.com">
+              <img className="icons email" src={email} alt="email logo" />
+            </a>
+          </div>
 
+          <div className={`header-right ${color}`}>
+            <HashLink
+              to="#home"
+              smooth
+              className={`rose left-most ${color}-left`}
+            >
+              home
+            </HashLink>
 
-                    <a href= "https://www.linkedin.com/in/ethan-luc-29035325a/"> 
-                        <img className = "icons" src = {linkedInLogo} alt = 'linkedin logo' />
-                    </a>
-                    
-                    <a href= "mailto:ethan.q.luc@gmail.com">
-                        <img className = "icons email" src = {email} alt = 'email logo' />
-                    </a>
-          
-                </div>
+            <HashLink 
+             to="#about" 
+             smooth 
+             className={`rose ${color}-middle`}
+             >
+              about
+            </HashLink>
 
-                <div className =  {`header-right ${color}`}>
-     
-                    <Link to= "#About" smooth className="link first">About</Link>
-                    <Link className = "link">Projects</Link>
-                    <Link className = "link">Experience</Link>
-                    
-                </div>
-
-
-
-            </div>
+            <HashLink
+              to="#projects"
+              smooth
+              className={`rose right-most ${color}-right `}
+            >
+              projects
+            </HashLink>
+          </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
