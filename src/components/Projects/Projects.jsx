a23bf18988a25f262c063e04689533f95c6a172b
ethanluc7 <140726164+ethanluc7@users.noreply.github.com>
2025-01-05 13:15:42 -0500

    Add files via upload

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../index.css';
+import './Projects.css';
+
+const Projects = ( {color}) => {
+  return (
+    <div>
+      <div className = "projects-container">
+
+        <div className = {`projects-header ${color}-middle`}>
+            projects
+        </div>
+      
+        </div>
+
+    </div>
+  )
+}
+
+export default Projects
